ea22241224f8a2e7d15254ae8b9f363ff505197c
ED <dikeemmanuel54@gmail.com>
2023-08-14 10:46:49 -0400

    order can fill

---
+++ netlify/functions/FufuillOrder.js
@@ -0,0 +1,50 @@
+// Set your secret key. Remember to switch to your live secret key in production.
+// See your keys here: https://dashboard.stripe.com/apikeys
+const stripe = require('stripe')(process.env.STRIPE_SECRET_API_KEY, {
+  apiVersion: '2020-08-27',
+  appInfo: { // For sample support and debugging, not required for production:
+    name: "stripe-samples/checkout-one-time-payments",
+    version: "0.0.1",
+    url: "https://github.com/stripe-samples/checkout-one-time-payments"
+  }
+});
+
+exports.handler = async ({ body, headers }) => {
+  try {
+    // check the webhook to make sure it’s valid
+    const stripeEvent = stripe.webhooks.constructEvent(
+      body,
+      headers['stripe-signature'],
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    // only do stuff if this is a successful Stripe Checkout purchase
+    /* if (stripeEvent.type === 'checkout.session.completed') {
+      const eventObject = stripeEvent.data.object;
+      const items = eventObject.display_items;
+      const shippingDetails = eventObject.shipping;
+
+      // Send and email to our fulfillment provider using Sendgrid.
+      const purchase = { items, shippingDetails };
+      const msg = {
+        to: process.env.FULFILLMENT_EMAIL_ADDRESS,
+        from: process.env.FROM_EMAIL_ADDRESS,
+        subject: `New purchase from ${shippingDetails.name}`,
+        text: JSON.stringify(purchase, null, 2),
+      };
+      await sgMail.send(msg);
+    } */
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ received: true }),
+    };
+  } catch (err) {
+    console.log(`Stripe webhook failed with ${err}`);
+
+    return {
+      statusCode: 400,
+      body: `Webhook Error: ${err.message}`,
+    };
+  }
+};
