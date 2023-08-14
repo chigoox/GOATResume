4b43ab6bf4993415afae153a5dca2a86bb36edfa
ED <dikeemmanuel54@gmail.com>
2023-08-14 14:07:20 -0400

    lol

--- src/Componets/Header/UserManager.jsx
+++ src/Componets/Header/UserManager.jsx
@@ -165,11 +165,10 @@ const UserManager = ({ loggedInUser, setLoggedInUser }) => {
         )
     }
 
-    console.log(loggedInUser)
 
     return (
         <div className='text-red-600 absolute right-0 top-2 center flex-col overflow-hidden  '>
-            <button className='relative top-1' onClick={toggleAccountPanel}>
+            <button className='relative top-1 md:top-0 left-20 md:left-0' onClick={toggleAccountPanel}>
                 <BsPersonFill color={'white'} size={24} />
             </button>
             <RegisterMenu setShowRegister={setShowRegister} setLoggedInUser={setLoggedInUser} />
