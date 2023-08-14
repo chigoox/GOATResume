4bcfe07f4e0647ff74ba1a1f146d966e899c0668
ED <dikeemmanuel54@gmail.com>
2023-08-14 13:39:59 -0400

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -62,19 +62,19 @@ function Home() {
                 <h1 className='text-white text-7xl font-bold  w-fit m-auto hover:scale-105 trans-slow'>Featured Portfolio</h1>
                 <div className='flex flex-col p-2   justify-center items-center md:flex-row gap-4 h-full w-full'>
                     <div className='h-60 w-full group overflow-hidden relative no-scrollbar md:h-96 md:w-96  hover:scale-105 trans rounded-3xl'>
-                        <a href='https://higherbeingset.netlify.app/' target="_blank" rel="noopener noreferrer" className='h-full w-full rounded-3xl bg-gradient-to-r flex-col flex justify-center items-center p-2 z- from-black to-gray-800 -500 absolute   hover:opacity-50 trans-slow opacity-95'>
-                            <h1 className='text-5xl md:text-left text-center font-bold text-white group-hover:text-opacity-0 trans-slow'>Higer Beings E.T</h1>
-                            <h1 className=' text-sm text-white group-hover:text-opacity-0 trans-slow'>Spiritual Website</h1>
+                        <a href='https://humainegrandeur.netlify.app/Shop' target="_blank" rel="noopener noreferrer" className='h-full w-full rounded-3xl bg-gradient-to-r flex-col flex justify-center items-center p-2 z- from-black to-gray-800 -500 absolute   hover:opacity-50 trans-slow opacity-95'>
+                            <h1 className='text-5xl md:text-left text-center font-bold text-white group-hover:text-opacity-0 trans-slow'>Humaine Grandeur</h1>
+                            <h1 className=' text-sm text-white group-hover:text-opacity-0 trans-slow'>Urban clothing line</h1>
                         </a>
-                        <iframe className='w-full h-full no-scrollbar' src="https://higherbeingset.netlify.app/" title="description"></iframe>
+                        <iframe className='w-full h-full no-scrollbar' src="https://humainegrandeur.netlify.app/Shop" title="description"></iframe>
                     </div>
                     <div className='flex flex-col  h-96 w-full md:w-fit  justify-center items-center gap-4'>
                         <button className='   w-full md:w-96 rounded-3xl group h-48 hover:scale-105 overflow-hidden relative trans'>
-                            <a href='https://humainegrandeur.netlify.app/Shop' target="_blank" rel="noopener noreferrer" className='h-full w-full rounded-3xl bg-gradient-to-r flex-col flex justify-center items-center p-2 z- from-black to-gray-800 -500 absolute   hover:opacity-50 trans-slow opacity-95'>
-                                <h1 className='text-5xl md:text-left text-center font-bold text-white group-hover:text-opacity-0 trans-slow'>Humaine Grandeur</h1>
-                                <h1 className=' text-sm text-white group-hover:text-opacity-0 trans-slow'>Urban clothing line</h1>
+                            <a href='https://5isthegoat.netlify.app/' target="_blank" rel="noopener noreferrer" className='h-full w-full rounded-3xl bg-gradient-to-r flex-col flex justify-center items-center p-2 z- from-black to-gray-800 -500 absolute   hover:opacity-50 trans-slow opacity-95'>
+                                <h1 className='text-5xl md:text-left text-center font-bold text-white group-hover:text-opacity-0 trans-slow'>5istheGoat</h1>
+                                <h1 className=' text-sm text-white group-hover:text-opacity-0 trans-slow'>my Old Website</h1>
                             </a>
-                            <iframe className='w-full h-full no-scrollbar' src="https://humainegrandeur.netlify.app/Shop" title="description"></iframe>
+                            <iframe className='w-full h-full no-scrollbar' src="https://5isthegoat.netlify.app/" title="description"></iframe>
                         </button>
                         <button className='bg-red-900 group w-full md:w-96 relative rounded-3xl h-48 hover:scale-105 trans overflow-hidden'>
                             <a href='https://voidcard.netlify.app/BV89o4Y7oveCwfnZCpFboVaC0b13' target="_blank" rel="noopener noreferrer" className='rounded-3xl h-full w-full bg-gradient-to-r flex-col flex justify-center items-center p-2  from-black to-rose-500 absolute   hover:opacity-50 trans-slow opacity-80'>
