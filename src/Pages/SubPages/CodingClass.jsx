ca5bc28732cf7665744ba579565a6c4c373eda58
ED <dikeemmanuel54@gmail.com>
2023-08-14 12:43:06 -0400

    update!

--- src/Pages/SubPages/CodingClass.jsx
+++ src/Pages/SubPages/CodingClass.jsx
@@ -5,10 +5,12 @@ const CodingClass = ({ onPage }) => {
         <div className='text-8xl text-white'>
 
             {onPage == 'Coding Class' &&
-                <div>
+                <div className='mb-52'>
+                    <h1 className='text-center text-4xl my-4'>Join My Coding Class Today</h1>
+
                     <stripe-pricing-table
-                        pricing-table-id="prctbl_1NetcxBiXRHCNCMj3wIkvlpS"
-                        publishable-key="pk_test_51Nend2BiXRHCNCMjlvVC6REAz6mYAzgBvvNSRPUj0YPCveLwtXpcwJmDz22pZb9fx46pgDjr3Oqv1PzYVLzq24DZ00uEtNVi8G"
+                        pricing-table-id="prctbl_1Nf3hKBiXRHCNCMjzNEr8Zon"
+                        publishable-key="pk_live_51Nend2BiXRHCNCMj3rHVP9lhAWQDRXvB0PjJ6t9ga4hpg8hn8grmoapR5fHynzWnpURcz3WmmeUxGYmz2a2Qahty0027vrCkRC"
                     >
                     </stripe-pricing-table>
                 </div>
